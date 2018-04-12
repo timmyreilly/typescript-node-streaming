fe935cd7cb3d20f8f52a58c5aa904f6ce1377931
timmyreilly <timreilly@live.com>
2018-04-12 15:24:26 -0700

    reduced

--- src/util/observe.ts
+++ src/util/observe.ts
@@ -321,10 +321,10 @@ function mainFour() {
 
     // Use one or more concatMap, map, and filter calls to create an array with the following items
     // [
-    //	 {"id": 675465, "title": "Fracture", "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
-    //	 {"id": 65432445, "title": "The Chamber", "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
-    //	 {"id": 654356453, "title": "Bad Boys", "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
-    //	 {"id": 70111470, "title": "Die Hard", "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+    // 	 {"id": 675465, "title": "Fracture", "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+    // 	 {"id": 65432445, "title": "The Chamber", "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+    // 	 {"id": 654356453, "title": "Bad Boys", "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
+    // 	 {"id": 70111470, "title": "Die Hard", "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
     // ];
 
     return movieLists.
@@ -338,10 +338,71 @@ function mainFour() {
                         map(function (boxart) {
                             return { id: video.id, title: video.title, boxart: boxart.url };
                         });
-                })
+                });
         }); // Complete this expression!
 
 }
 
 console.log("<----main four --->");
 console.log(mainFour());
+
+// [1,2,3].reduce(function(accumulatedValue, currentValue) { return accumulatedValue + currentValue; }); === [6];
+// [1,2,3].reduce(function(accumulatedValue, currentValue) { return accumulatedValue + currentValue; }, 10); === [16];
+
+Array.prototype.reduce = function (combiner, initialValue) {
+    let counter,
+        accumulatedValue;
+
+    // If the array is empty, do nothing
+    if (this.length === 0) {
+        return this;
+    }
+    else {
+        // If the user didn't pass an initial value, use the first item.
+        if (arguments.length === 1) {
+            counter = 1;
+            accumulatedValue = this[0];
+        }
+        else if (arguments.length >= 2) {
+            counter = 0;
+            accumulatedValue = initialValue;
+        }
+        else {
+            throw "Invalid arguments.";
+        }
+
+        // Loop through the array, feeding the current value and the result of
+        // the previous computation back into the combiner function until
+        // we've exhausted the entire array and are left with only one value.
+        while (counter < this.length) {
+            accumulatedValue = combiner(accumulatedValue, this[counter]);
+            counter++;
+        }
+
+        return [accumulatedValue];
+    }
+};
+
+function mainFive() {
+    const boxarts = [
+        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+        { width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+        { width: 425, height: 150, url: "http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" }
+    ];
+
+    // You should return an array containing only the URL of the largest box art. Remember that reduce always
+    // returns an array with one item.
+    return boxarts.
+        reduce(function (acc, curr) {
+            const x = curr.height * curr.width;
+            if (x > acc) {
+                return x;
+            } else {
+                return curr;
+            }
+        }).map(function (item) { return item.url; });   // Complete this expression
+}
+
+console.log("main 5 >>>>>>>>");
+console.log(mainFive());
